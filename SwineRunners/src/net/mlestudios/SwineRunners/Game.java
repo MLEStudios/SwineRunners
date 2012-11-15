cd081cb4cc920eb46622a576cd8ff7c85ae667fd
MatthewEnderle <mattenderle488@gmail.com>
2012-11-14 23:42:20 -0600

    Nulled Spawning, Added Game Listener

---
+++ SwineRunners/src/net/mlestudios/SwineRunners/Game.java
@@ -0,0 +1,11 @@
+package net.mlestudios.SwineRunners;
+
+import org.bukkit.entity.Player;
+
+public class Game extends Main {
+
+	public static void inGame(Player player, int a)
+	{
+		
+	}
+}
--- SwineRunners/src/net/mlestudios/SwineRunners/SwineHerd.java
+++ SwineRunners/src/net/mlestudios/SwineRunners/SwineHerd.java
@@ -1,7 +1,11 @@
 package net.mlestudios.SwineRunners;
 
+import java.util.Vector;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.craftbukkit.entity.CraftEntity;
+import org.bukkit.entity.CreatureType;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
@@ -9,9 +13,13 @@ import net.minecraft.server.World;
 
 public class SwineHerd extends Main {
 
+	private static Vector<String> pigs;
+	
 	public static void spawnPig(Player player)
 	{
 
-		player.getWorld().spawnEntity(player.getLocation(), EntityType.PIG);
+		//player.getWorld().spawnEntity(player.getLocation(), EntityType.PIG);
+		//((CraftEntity)player).getHandle().setPassengerOf(((CraftEntity)player.getNearbyEntities(1, 1, 1)).getHandle());
+		// player.getNearbyEntities(1, 1, 1);
 	}
 }
